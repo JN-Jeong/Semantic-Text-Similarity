1132caf3629de1c55c1dc1e27d5d0656445ad64c
root <jjn00712@gmail.com>
2022-11-10 01:13:28 +0000

    refactor: Dataloader 간략화

--- data_loader/data_loaders.py
+++ data_loader/data_loaders.py
@@ -109,81 +109,81 @@ class Dataloader(pl.LightningDataModule):
         self.delete_columns = ["id"]
         self.text_columns = ["sentence_1", "sentence_2"]
 
-    def tokenizing(self, dataframe, swap):
-        data = []
-        for idx, item in tqdm(dataframe.iterrows(), desc="tokenizing", total=len(dataframe)):
-            text = "[SEP]".join([item[text_column] for text_column in self.text_columns])
-            if self.use_preprocessing:
-                text = utils.text_preprocessing(text)  # 전처리 추가
-            outputs = self.tokenizer(text, add_special_tokens=True, padding="max_length", truncation=True)
-            data.append(outputs["input_ids"])
+    # def tokenizing(self, dataframe, swap):
+    #     data = []
+    #     for idx, item in tqdm(dataframe.iterrows(), desc="tokenizing", total=len(dataframe)):
+    #         text = "[SEP]".join([item[text_column] for text_column in self.text_columns])
+    #         if self.use_preprocessing:
+    #             text = utils.text_preprocessing(text)  # 전처리 추가
+    #         outputs = self.tokenizer(text, add_special_tokens=True, padding="max_length", truncation=True)
+    #         data.append(outputs["input_ids"])
 
-        if swap:  # swap 적용시 양방향 될 수 있도록
-            for idx, item in tqdm(dataframe.iterrows(), desc="tokenizing", total=len(dataframe)):
-                text = "[SEP]".join([item[text_column] for text_column in self.text_columns[::-1]])
+    #     if swap:  # swap 적용시 양방향 될 수 있도록
+    #         for idx, item in tqdm(dataframe.iterrows(), desc="tokenizing", total=len(dataframe)):
+    #             text = "[SEP]".join([item[text_column] for text_column in self.text_columns[::-1]])
 
-                if self.use_preprocessing:
-                    text = utils.text_preprocessing(text)  # 전처리 추가
-                outputs = self.tokenizer(text, add_special_tokens=True, padding="max_length", truncation=True)
-                data.append(outputs["input_ids"])
+    #             if self.use_preprocessing:
+    #                 text = utils.text_preprocessing(text)  # 전처리 추가
+    #             outputs = self.tokenizer(text, add_special_tokens=True, padding="max_length", truncation=True)
+    #             data.append(outputs["input_ids"])
 
-        return data
+    #     return data
 
-    def preprocessing(self, data, swap):
-        data = data.drop(columns=self.delete_columns)  # id column 삭제
+    # def preprocessing(self, data, swap):
+    #     data = data.drop(columns=self.delete_columns)  # id column 삭제
 
-        try:
-            if swap:
-                targets = data[self.target_columns].values.tolist() + data[self.target_columns].values.tolist()
-            else:
-                targets = data[self.target_columns].values.tolist()
-        except:
-            targets = []
-        inputs = self.tokenizing(data, swap)
+    #     try:
+    #         if swap:
+    #             targets = data[self.target_columns].values.tolist() + data[self.target_columns].values.tolist()
+    #         else:
+    #             targets = data[self.target_columns].values.tolist()
+    #     except:
+    #         targets = []
+    #     inputs = self.tokenizing(data, swap)
 
-        return inputs, targets
+    #     return inputs, targets
 
-    def setup(self, stage="fit"):
-        if stage == "fit":
-            total_data = pd.read_csv(self.train_path)
+    # def setup(self, stage="fit"):
+    #     if stage == "fit":
+    #         total_data = pd.read_csv(self.train_path)
 
-            split = StratifiedShuffleSplit(n_splits=1, test_size=1 - self.train_ratio, random_state=1004)  # 층화 추출 fix
-            for train_idx, val_idx in split.split(total_data, total_data["binary-label"]):
-                train_data = total_data.loc[train_idx]
-                val_data = total_data.loc[val_idx]
+    #         split = StratifiedShuffleSplit(n_splits=1, test_size=1 - self.train_ratio, random_state=1004)  # 층화 추출 fix
+    #         for train_idx, val_idx in split.split(total_data, total_data["binary-label"]):
+    #             train_data = total_data.loc[train_idx]
+    #             val_data = total_data.loc[val_idx]
 
-            # train_data = total_data.sample(frac=self.train_ratio)
-            # val_data = total_data.drop(train_data.index)
+    #         # train_data = total_data.sample(frac=self.train_ratio)
+    #         # val_data = total_data.drop(train_data.index)
 
-            train_inputs, train_targets = self.preprocessing(train_data, self.swap)
-            val_inputs, val_targets = self.preprocessing(val_data, self.swap)
-            print("train data len : ", len(train_inputs))
-            print("valid data len : ", len(val_inputs))
+    #         train_inputs, train_targets = self.preprocessing(train_data, self.swap)
+    #         val_inputs, val_targets = self.preprocessing(val_data, self.swap)
+    #         print("train data len : ", len(train_inputs))
+    #         print("valid data len : ", len(val_inputs))
 
-            self.train_dataset = Dataset(train_inputs, train_targets)
-            self.val_dataset = Dataset(val_inputs, val_targets)
+    #         self.train_dataset = Dataset(train_inputs, train_targets)
+    #         self.val_dataset = Dataset(val_inputs, val_targets)
 
-        else:
-            test_data = pd.read_csv(self.test_path)
-            predict_data = pd.read_csv(self.predict_path)
+    #     else:
+    #         test_data = pd.read_csv(self.test_path)
+    #         predict_data = pd.read_csv(self.predict_path)
 
-            test_inputs, test_targets = self.preprocessing(test_data, False)
-            predict_inputs, predict_targets = self.preprocessing(predict_data, False)
+    #         test_inputs, test_targets = self.preprocessing(test_data, False)
+    #         predict_inputs, predict_targets = self.preprocessing(predict_data, False)
 
-            self.test_dataset = Dataset(test_inputs, test_targets)
-            self.predict_dataset = Dataset(predict_inputs, predict_targets)
+    #         self.test_dataset = Dataset(test_inputs, test_targets)
+    #         self.predict_dataset = Dataset(predict_inputs, predict_targets)
 
-    def train_dataloader(self):
-        return torch.utils.data.DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=self.shuffle)
+    # def train_dataloader(self):
+    #     return torch.utils.data.DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=self.shuffle)
 
-    def val_dataloader(self):
-        return torch.utils.data.DataLoader(self.val_dataset, batch_size=self.batch_size)
+    # def val_dataloader(self):
+    #     return torch.utils.data.DataLoader(self.val_dataset, batch_size=self.batch_size)
 
-    def test_dataloader(self):
-        return torch.utils.data.DataLoader(self.test_dataset, batch_size=self.batch_size)
+    # def test_dataloader(self):
+    #     return torch.utils.data.DataLoader(self.test_dataset, batch_size=self.batch_size)
 
-    def predict_dataloader(self):
-        return torch.utils.data.DataLoader(self.predict_dataset, batch_size=self.batch_size)
+    # def predict_dataloader(self):
+    #     return torch.utils.data.DataLoader(self.predict_dataset, batch_size=self.batch_size)
 
     def new_vocab_size(self):
         return self.new_token_count + self.tokenizer.vocab_size
